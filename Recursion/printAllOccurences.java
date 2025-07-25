c0d0f5d9b9e42588bbbe547f42e543b10ee843e6
Shyna-Garg <shynugrg571@gmail.com>
2025-07-25 16:37:49 +0530

    changes

--- Recursion/printAllOccurences.java
+++ Recursion/printAllOccurences.java
@@ -2,20 +2,24 @@ package Recursion;
 
 //to print all the index of the occurences of key in given array 
 public class printAllOccurences {
-    public static int allOccurences(int arr[], int key, int i) {
+    public static void allOccurences(int arr[], int key, int i,boolean found) {
         if (i == arr.length) {
+            if(!found){
             System.out.println("key not found");
+            }
+            return;
         }
-        if (key == arr[i]) {
-            return i;
+         if (key == arr[i]) {
+            System.out.println(i);
+            found = true;
             
         }
-        return allOccurences(arr, key, i + 1);
+     allOccurences(arr, key, i + 1,found);
     }
 
     public static void main(String[] args) {
-        int arr[] = { 1, 2, 5, 3, 5, 4, 6, 5 };
-        System.out.println(allOccurences(arr, 5, 0));
+        int arr[] = { 1, 2, 5, 3, 5, 4, 6, 5,3 };               // 2 4 7
+     allOccurences(arr, 5, 0,false);
     }
 
 }
