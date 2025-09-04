bb0d83016af1d36d0a77964737afa69c61e68a38
Shyna-Garg <shynugrg571@gmail.com>
2025-09-04 11:16:09 +0530

    changes

---
+++ Divide_and_Conquer/quickSort.java
@@ -0,0 +1,14 @@
+package Divide_and_Conquer;
+
+//will use pivot and partition method
+public class quickSort {
+    
+    public static void printArr(int arr[] , int  si , int ei){
+        
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+}
